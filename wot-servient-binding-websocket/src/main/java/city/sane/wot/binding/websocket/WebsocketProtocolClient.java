22bf65a4f9fabfff7a28e0e45ab39007bd75defb
Marten Kiehn <3kiehn@informatik.uni-hamburg.de>
2019-11-18 15:20:06 +0100

    add TODOs

--- wot-servient-binding-websocket/src/main/java/city/sane/wot/binding/websocket/WebsocketProtocolClient.java
+++ wot-servient-binding-websocket/src/main/java/city/sane/wot/binding/websocket/WebsocketProtocolClient.java
@@ -2,14 +2,25 @@ package city.sane.wot.binding.websocket;
 
 import city.sane.wot.binding.ProtocolClient;
 import city.sane.wot.binding.ProtocolClientException;
+import city.sane.wot.content.Content;
+import city.sane.wot.thing.form.Form;
+import city.sane.wot.thing.observer.Observer;
+import city.sane.wot.thing.observer.Subscription;
 import com.typesafe.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
 
 public class WebsocketProtocolClient implements ProtocolClient {
     final static Logger log = LoggerFactory.getLogger(WebsocketProtocolClient.class);
 
     public WebsocketProtocolClient(Config config) throws ProtocolClientException {
 
+    }
+
+    // TODO CompletableFuture subscribeResource(Form form, Observer<Content> observer)
+    public CompletableFuture<Subscription> subscribeResource(Form form, Observer<Content> observer) {
+        return null;
     }
 }
