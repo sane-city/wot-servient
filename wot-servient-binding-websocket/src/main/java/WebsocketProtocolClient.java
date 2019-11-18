64e161865ba1d75503c96d4e63e8c93245ceb7df
Marten Kiehn <3kiehn@informatik.uni-hamburg.de>
2019-11-18 13:55:32 +0100

    method body finished WebsocketClientFactrory

--- wot-servient-binding-websocket/src/main/java/WebsocketProtocolClient.java
+++ wot-servient-binding-websocket/src/main/java/WebsocketProtocolClient.java
@@ -1,4 +1,13 @@
 import city.sane.wot.binding.ProtocolClient;
+import city.sane.wot.binding.ProtocolClientException;
+import com.typesafe.config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WebsocketProtocolClient implements ProtocolClient {
+    final static Logger log = LoggerFactory.getLogger(WebsocketProtocolClient.class);
+
+    public WebsocketProtocolClient(Config config) throws ProtocolClientException {
+
+    }
 }
--- wot-servient-binding-websocket/src/main/java/WebsocketProtocolClientFactory.java
+++ wot-servient-binding-websocket/src/main/java/WebsocketProtocolClientFactory.java
@@ -1,27 +1,27 @@
 import city.sane.wot.binding.ProtocolClient;
 import city.sane.wot.binding.ProtocolClientException;
 import city.sane.wot.binding.ProtocolClientFactory;
+import com.typesafe.config.Config;
 
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Creates new {@link WebsocketProtocolClient} instances.
+ */
 public class WebsocketProtocolClientFactory implements ProtocolClientFactory {
+    private final Config config;
+
+    public WebsocketProtocolClientFactory(Config config) {
+        this.config = config;
+    }
+
     @Override
     public String getScheme() {
-        return null;
+        return "websocket";
     }
 
     @Override
     public ProtocolClient getClient() throws ProtocolClientException {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Void> init() {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Void> destroy() {
-        return null;
+        return new WebsocketProtocolClient(config);
     }
 }
