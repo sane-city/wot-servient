0eca6e911c7475a00c4d2531286540ea8d89b1a1
Marten Kiehn <3kiehn@informatik.uni-hamburg.de>
2019-11-18 15:20:06 +0100

    add TODOs

--- wot-servient-binding-websocket/src/main/java/city/sane/wot/binding/websocket/WebsocketProtocolServer.java
+++ wot-servient-binding-websocket/src/main/java/city/sane/wot/binding/websocket/WebsocketProtocolServer.java
@@ -9,21 +9,25 @@ import java.util.concurrent.CompletableFuture;
 
 public class WebsocketProtocolServer implements ProtocolServer {
 
+    // TODO CompletableFuture start()
     @Override
     public CompletableFuture<Void> start() {
         return null;
     }
 
+    //TODO CompletableFuture stop()
     @Override
     public CompletableFuture<Void> stop() {
         return null;
     }
 
+    //TODO CompletableFuture expose()
     @Override
     public CompletableFuture<Void> expose(ExposedThing thing) {
         return null;
     }
 
+    //TODO CompletableFuture destroy()
     @Override
     public CompletableFuture<Void> destroy(ExposedThing thing) {
         return null;
