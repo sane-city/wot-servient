d70f19d249834bd15f71caaaaef400938086d54c
Marten Kiehn <3kiehn@informatik.uni-hamburg.de>
2019-11-28 14:52:12 +0100

    add variables

--- wot-servient-binding-websocket/src/main/java/city/sane/wot/binding/websocket/message/ReadProperty.java
+++ wot-servient-binding-websocket/src/main/java/city/sane/wot/binding/websocket/message/ReadProperty.java
@@ -1,6 +1,15 @@
 package city.sane.wot.binding.websocket.message;
 
+import city.sane.wot.thing.ExposedThing;
+
+import java.util.Map;
+
 public class ReadProperty extends AbstractMessage {
+    private final Map<String, ExposedThing> things;
+
+    public ReadProperty(Map<String, ExposedThing> things) {
+        this.things = things;
+    }
 
     @Override
     public String getType() {
