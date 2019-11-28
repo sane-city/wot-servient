sane-city/wot-servient

commit c978d9d5dd768ba139ea2c17d086f207167436b6

Post request parameters to log

wot-servient-binding-http/src/main/java/city/sane/wot/binding/http/route/InvokeActionRoute.java
@@ -29,6 +29,9 @@ public class InvokeActionRoute extends AbstractRoute {
     @Override
     public Object handle(Request request, Response response) throws Exception {
         log.info("Handle {} to '{}'", request.requestMethod(), request.url());
+        if (!request.raw().getQueryString().isEmpty()) {
+            log.info("Request parameters: {}", request.raw().getQueryString());
+        }
 
         String requestContentType = getOrDefaultRequestContentType(request);
         if (!ContentManager.isSupportedMediaType(requestContentType)) {

commit fde9d56ab1e0536599024cb1e028428d39b97484

Fix regression caused by c978d9d5dd768ba139ea2c17d086f207167436b6

wot-servient-binding-http/src/main/java/city/sane/wot/binding/http/route/InvokeActionRoute.java
@@ -29,7 +29,7 @@ public class InvokeActionRoute extends AbstractRoute {
     @Override
     public Object handle(Request request, Response response) throws Exception {
         log.info("Handle {} to '{}'", request.requestMethod(), request.url());
-        if (!request.raw().getQueryString().isEmpty()) {
+        if (request.raw().getQueryString() != null && !request.raw().getQueryString().isEmpty()) {
             log.info("Request parameters: {}", request.raw().getQueryString());
         }
 