577b7e1993c2e07c16f9d76a32437d307f301971
Marten Kiehn <3kiehn@informatik.uni-hamburg.de>
2019-11-28 14:52:12 +0100

    add variables

--- wot-servient-binding-websocket/src/main/java/city/sane/wot/binding/websocket/WebsocketProtocolClient.java
+++ wot-servient-binding-websocket/src/main/java/city/sane/wot/binding/websocket/WebsocketProtocolClient.java
@@ -7,24 +7,56 @@ import city.sane.wot.thing.form.Form;
 import city.sane.wot.thing.observer.Observer;
 import city.sane.wot.thing.observer.Subscription;
 import com.typesafe.config.Config;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.concurrent.CompletableFuture;
 
 public class WebsocketProtocolClient implements ProtocolClient {
     final static Logger log = LoggerFactory.getLogger(WebsocketProtocolClient.class);
+    private final WebSocketClient cc;
 
-    public WebsocketProtocolClient(Config config) throws ProtocolClientException {
+    public WebsocketProtocolClient(Config config) throws ProtocolClientException, URISyntaxException {
+        cc = new WebSocketClient(new URI("ws://localhost:8080")) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                cc.connect();
+                log.info("onOpen status=" + serverHandshake.getHttpStatus() + ", statusMsg=" + serverHandshake.getHttpStatusMessage());
+            }
+
+            @Override
+            public void onMessage(String s) {
+                log.info("onMessage message= " + s);
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+
+            }
+
+            @Override
+            public void onError(Exception e) {
+                log.info(e.getMessage());
+            }
+        };
     }
 
     @Override
     public CompletableFuture<Content> readResource(Form form) {
-        return null;
+        // TODO
+        return CompletableFuture.supplyAsync(() -> {
+            cc.send("test read");
+            return null;
+        });
     }
 
     @Override
     public CompletableFuture<Content> writeResource(Form form, Content content) {
+        cc.send("test write");
         return null;
     }
 
