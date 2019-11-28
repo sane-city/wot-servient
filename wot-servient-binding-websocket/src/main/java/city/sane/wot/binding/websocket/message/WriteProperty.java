080c3639e6b50f30b1ea1b398ca9e313f8cb33f0
Marten Kiehn <3kiehn@informatik.uni-hamburg.de>
2019-11-28 16:31:56 +0100

    add stuff for WriteProperty.java

--- wot-servient-binding-websocket/src/main/java/city/sane/wot/binding/websocket/message/WriteProperty.java
+++ wot-servient-binding-websocket/src/main/java/city/sane/wot/binding/websocket/message/WriteProperty.java
@@ -1,8 +1,25 @@
 package city.sane.wot.binding.websocket.message;
 
 public class WriteProperty extends AbstractMessage {
+
+    private final String thingId;
+    private final String name;
+
+    public WriteProperty(String thingId, String name) {
+        this.thingId = thingId;
+        this.name = name;
+    }
+
     @Override
     public String getType() {
         return "writeProperty";
+    }
+
+    public String getThingId() {
+        return thingId;
+    }
+
+    public String getName() {
+        return name;
     }
 }
