2299f1420ad4acbdc16d396cb1dbf01253d3c2a2
Marten Kiehn <3kiehn@informatik.uni-hamburg.de>
2019-12-02 15:27:51 +0100

    private constructor

--- wot-servient/src/main/java/city/sane/wot/content/Content.java
+++ wot-servient/src/main/java/city/sane/wot/content/Content.java
@@ -9,6 +9,11 @@ public class Content implements Serializable {
     private final String type;
     private final byte[] body;
 
+    private Content() {
+        this.type = null;
+        this.body = null;
+    }
+
     public Content(String type, byte[] body) {
         this.type = type;
         this.body = body;
