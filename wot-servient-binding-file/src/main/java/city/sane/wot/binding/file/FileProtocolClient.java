d16946f9b22609f00095b209ff60d10419dcef3f
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2019-12-05 13:27:53 +0100

    Remove unused import

--- wot-servient-binding-file/src/main/java/city/sane/wot/binding/file/FileProtocolClient.java
+++ wot-servient-binding-file/src/main/java/city/sane/wot/binding/file/FileProtocolClient.java
@@ -13,7 +13,6 @@ import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
