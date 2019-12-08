a118839b12756c6564a5cf7c3f0cde838bd2ef17
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2019-12-09 00:49:26 +0100

    Separate Jadex Tests

--- wot-servient-binding-jadex/src/test/java/city/sane/wot/binding/jadex/ThingAgentTest.java
+++ wot-servient-binding-jadex/src/test/java/city/sane/wot/binding/jadex/ThingAgentTest.java
@@ -1,13 +1,11 @@
-package city.sane.wot.thing;
+package city.sane.wot.binding.jadex;
 
 import city.sane.wot.Servient;
 import city.sane.wot.ServientException;
-import city.sane.wot.binding.jadex.JadexProtocolClientFactory;
-import city.sane.wot.binding.jadex.JadexProtocolServer;
-import city.sane.wot.thing.action.ConsumedThingAction;
+import city.sane.wot.thing.ConsumedThing;
+import city.sane.wot.thing.ExposedThing;
 import city.sane.wot.thing.action.ThingAction;
 import city.sane.wot.thing.event.ThingEvent;
-import city.sane.wot.thing.observer.Subscription;
 import city.sane.wot.thing.property.ConsumedThingProperty;
 import city.sane.wot.thing.property.ThingProperty;
 import city.sane.wot.thing.schema.IntegerSchema;
@@ -16,20 +14,16 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Collections;
 import java.util.Date;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.assertEquals;
 
-@Ignore("Jadex platform discovery is unstable")
-public class JadexProtocolTest {
+public class ThingAgentTest {
     private Servient servient;
 
     @Before
@@ -73,47 +67,6 @@ public class JadexProtocolTest {
         assertEquals(1337, counter.read().get());
     }
 
-    @Test(timeout = 20 * 1000)
-    public void observeProperty() throws ConsumedThingException, InterruptedException, ExecutionException {
-        ExposedThing exposedThing = getExposedCounterThing();
-        servient.addThing(exposedThing);
-        exposedThing.expose().join();
-
-        ConsumedThing consumedThing = new ConsumedThing(servient, exposedThing);
-
-        AtomicInteger counter1 = new AtomicInteger();
-        Subscription subscription1 = consumedThing.getProperty("count").subscribe(
-                next -> counter1.getAndIncrement()
-        ).get();
-
-        AtomicInteger counter2 = new AtomicInteger();
-        Subscription subscription2 = consumedThing.getProperty("count").subscribe(
-                next -> counter2.getAndIncrement()
-        ).get();
-
-        // wait until client establish subscription
-        // TODO: This is error-prone. We need a feature that notifies us when the subscription is active.
-        Thread.sleep(5 * 1000L);
-
-        exposedThing.getProperty("count").write(1337).join();
-
-        // wait until client fires next subscribe-request to server
-        // TODO: This is error-prone. We need a feature that notifies us when the subscription is active.
-        Thread.sleep(5 * 1000L);
-
-        exposedThing.getProperty("count").write(1338).join();
-
-        // Subscriptions are executed asynchronously. Therefore, wait "some" time before we check the result.
-        // TODO: This is error-prone. We need a function that notifies us when all subscriptions have been executed.
-        Thread.sleep(5 * 1000L);
-
-        subscription1.unsubscribe();
-        subscription2.unsubscribe();
-
-        assertEquals(2, counter1.get());
-        assertEquals(2, counter2.get());
-    }
-
     @Test
     public void readProperties() throws ExecutionException, InterruptedException {
         ExposedThing exposedThing = getExposedCounterThing();
@@ -138,61 +91,6 @@ public class JadexProtocolTest {
         Map values = thing.readProperties(Collections.singletonList("count")).get();
         assertEquals(1, values.size());
         assertEquals(42, values.get("count"));
-    }
-
-    @Test
-    public void invokeAction() throws ExecutionException, InterruptedException {
-        ExposedThing exposedThing = getExposedCounterThing();
-        servient.addThing(exposedThing);
-        exposedThing.expose().join();
-
-        ConsumedThing thing = new ConsumedThing(servient, exposedThing);
-
-        ConsumedThingAction increment = thing.getAction("increment");
-
-        CompletableFuture future = increment.invoke(Map.of("step", 3));
-        assertEquals(45, future.get());
-    }
-
-    @Test(timeout = 20 * 1000)
-    public void emitEvent() throws ConsumedThingException, InterruptedException, ExecutionException {
-        ExposedThing exposedThing = getExposedCounterThing();
-        servient.addThing(exposedThing);
-        exposedThing.expose().join();
-
-        ConsumedThing consumedThing = new ConsumedThing(servient, exposedThing);
-
-        AtomicInteger counter1 = new AtomicInteger();
-        Subscription subscription1 = consumedThing.getEvent("change").subscribe(
-                next -> counter1.getAndIncrement()
-        ).get();
-
-        AtomicInteger counter2 = new AtomicInteger();
-        Subscription subscription2 = consumedThing.getEvent("change").subscribe(
-                next -> counter2.getAndIncrement()
-        ).get();
-
-        // wait until client establish subscription
-        // TODO: This is error-prone. We need a feature that notifies us when the subscription is active.
-        Thread.sleep(5 * 1000L);
-
-        exposedThing.getEvent("change").emit();
-
-        // wait until client fires next subscribe-request to server
-        // TODO: This is error-prone. We need a feature that notifies us when the subscription is active.
-        Thread.sleep(5 * 1000L);
-
-        exposedThing.getEvent("change").emit();
-
-        // Subscriptions are executed asynchronously. Therefore, wait "some" time before we check the result.
-        // TODO: This is error-prone. We need a function that notifies us when all subscriptions have been executed.
-        Thread.sleep(5 * 1000L);
-
-        subscription1.unsubscribe();
-        subscription2.unsubscribe();
-
-        assertEquals(2, counter1.get());
-        assertEquals(2, counter2.get());
     }
 
     private ExposedThing getExposedCounterThing() {
