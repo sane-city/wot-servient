55344d668fa8b7a901876505d9a39b30eda95dd5
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2019-12-09 21:30:15 +0100

    Remove Servient from mqtt tests

---
+++ wot-servient-binding-mqtt/src/test/java/city/sane/wot/binding/mqtt/MqttProtocolServerTest.java
@@ -0,0 +1,119 @@
+package city.sane.wot.binding.mqtt;
+
+import city.sane.wot.binding.ProtocolServerException;
+import city.sane.wot.thing.ExposedThing;
+import city.sane.wot.thing.action.ThingAction;
+import city.sane.wot.thing.event.ThingEvent;
+import city.sane.wot.thing.property.ThingProperty;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MqttProtocolServerTest {
+    private MqttProtocolServer server;
+    private Config config;
+
+    @Before
+    public void setUp() throws ProtocolServerException {
+        config = ConfigFactory.load();
+        server = new MqttProtocolServer(config);
+        server.start().join();
+    }
+
+    @After
+    public void tearDown() {
+        server.stop().join();
+    }
+
+    @Test
+    public void expose() {
+        ExposedThing thing = getCounterThing();
+        server.expose(thing).join();
+
+        assertTrue("There must be at least one form", !thing.getProperty("count").getForms().isEmpty());
+        assertTrue("There must be at least one action", !thing.getAction("increment").getForms().isEmpty());
+        assertTrue("There must be at least one event", !thing.getEvent("change").getForms().isEmpty());
+    }
+
+    @Test
+    public void invokeAction() throws MqttException, ExecutionException, InterruptedException {
+        ExposedThing thing = getCounterThing();
+        server.expose(thing).join();
+
+        MqttClient client = new MqttClient(config.getString("wot.servient.mqtt.broker"), MqttClient.generateClientId());
+        client.connect();
+        client.publish("counter/actions/increment", new MqttMessage());
+
+        // Invoked action is executed asynchronously. Wait some time to make sure that the execution has finished.
+        Thread.sleep(1 * 1000L);
+
+        assertEquals(43, thing.getProperty("count").read().get());
+    }
+
+    private ExposedThing getCounterThing() {
+        ThingProperty counterProperty = new ThingProperty.Builder()
+                .setType("integer")
+                .setDescription("current counter value")
+                .setObservable(true)
+                .setReadOnly(true)
+                .build();
+
+        ThingProperty lastChangeProperty = new ThingProperty.Builder()
+                .setType("string")
+                .setDescription("last change of counter value")
+                .setObservable(true)
+                .setReadOnly(true)
+                .build();
+
+        ExposedThing thing = new ExposedThing(null)
+                .setId("counter")
+                .setTitle("counter")
+                .setDescription("counter example Thing");
+
+        thing.addProperty("count", counterProperty, 42);
+        thing.addProperty("lastChange", lastChangeProperty, new Date().toString());
+
+        thing.addAction("increment", new ThingAction(), (input, options) -> {
+            return thing.getProperty("count").read().thenApply(value -> {
+                int newValue = ((Integer) value) + 1;
+                thing.getProperty("count").write(newValue);
+                thing.getProperty("lastChange").write(new Date().toString());
+                thing.getEvent("change").emit();
+                return newValue;
+            });
+        });
+
+        thing.addAction("decrement", new ThingAction(), (input, options) -> {
+            return thing.getProperty("count").read().thenApply(value -> {
+                int newValue = ((Integer) value) - 1;
+                thing.getProperty("count").write(newValue);
+                thing.getProperty("lastChange").write(new Date().toString());
+                thing.getEvent("change").emit();
+                return newValue;
+            });
+        });
+
+        thing.addAction("reset", new ThingAction(), (input, options) -> {
+            return thing.getProperty("count").write(0).thenApply(value -> {
+                thing.getProperty("lastChange").write(new Date().toString());
+                thing.getEvent("change").emit();
+                return 0;
+            });
+        });
+
+        thing.addEvent("change", new ThingEvent());
+
+        return thing;
+    }
+}
