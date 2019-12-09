b152d7526b7eb888bdb22177494bb06f4c0aa425
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2019-12-09 01:15:17 +0100

    Fix

--- wot-servient-binding-http/src/test/java/city/sane/wot/binding/http/route/InvokeActionRouteTest.java
+++ wot-servient-binding-http/src/test/java/city/sane/wot/binding/http/route/InvokeActionRouteTest.java
@@ -6,6 +6,9 @@ import city.sane.wot.content.Content;
 import city.sane.wot.content.ContentCodecException;
 import city.sane.wot.content.ContentManager;
 import city.sane.wot.thing.ExposedThing;
+import city.sane.wot.thing.action.ThingAction;
+import city.sane.wot.thing.event.ThingEvent;
+import city.sane.wot.thing.property.ThingProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
@@ -21,6 +24,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -90,5 +94,61 @@ public class InvokeActionRouteTest {
         assertEquals(200, response.getStatusLine().getStatusCode());
         assertEquals("application/json", ContentType.getOrDefault(response.getEntity()).getMimeType());
         assertEquals("45", EntityUtils.toString(response.getEntity()));
+    }
+
+    private ExposedThing getCounterThing() {
+        ThingProperty counterProperty = new ThingProperty.Builder()
+                .setType("integer")
+                .setDescription("current counter value")
+                .setObservable(true)
+                .setReadOnly(true)
+                .build();
+
+        ThingProperty lastChangeProperty = new ThingProperty.Builder()
+                .setType("string")
+                .setDescription("last change of counter value")
+                .setObservable(true)
+                .setReadOnly(true)
+                .build();
+
+        ExposedThing thing = new ExposedThing(null)
+                .setId("counter")
+                .setTitle("counter")
+                .setDescription("counter example Thing");
+
+        thing.addProperty("count", counterProperty, 42);
+        thing.addProperty("lastChange", lastChangeProperty, new Date().toString());
+
+        thing.addAction("increment", new ThingAction(), (input, options) -> {
+            return thing.getProperty("count").read().thenApply(value -> {
+                int newValue = ((Integer) value) + 1;
+                thing.getProperty("count").write(newValue);
+                thing.getProperty("lastChange").write(new Date().toString());
+                thing.getEvent("change").emit();
+                return newValue;
+            });
+        });
+
+        thing.addAction("decrement", new ThingAction(), (input, options) -> {
+            return thing.getProperty("count").read().thenApply(value -> {
+                int newValue = ((Integer) value) - 1;
+                thing.getProperty("count").write(newValue);
+                thing.getProperty("lastChange").write(new Date().toString());
+                thing.getEvent("change").emit();
+                return newValue;
+            });
+        });
+
+        thing.addAction("reset", new ThingAction(), (input, options) -> {
+            return thing.getProperty("count").write(0).thenApply(value -> {
+                thing.getProperty("lastChange").write(new Date().toString());
+                thing.getEvent("change").emit();
+                return 0;
+            });
+        });
+
+        thing.addEvent("change", new ThingEvent());
+
+        return thing;
     }
 }
