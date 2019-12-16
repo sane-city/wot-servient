b2cc62383ec962463fb3097c7881160c5f7553a8
Lukas von Thun <@8thun>
2019-12-16 15:12:52 +0100

    WriteProperty positiv

--- wot-servient-binding-websocket/src/test/java/city/sane/wot/binding/websocket/message/WritePropertyTest.java
+++ wot-servient-binding-websocket/src/test/java/city/sane/wot/binding/websocket/message/WritePropertyTest.java
@@ -1,5 +1,8 @@
 package city.sane.wot.binding.websocket.message;
 
+import city.sane.wot.content.Content;
+import city.sane.wot.content.ContentCodecException;
+import city.sane.wot.content.ContentManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,9 +10,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class WritePropertyTest {
-
+    private WriteProperty wp;
+    private Content c;
     @Before
     public void setUp() throws Exception {
+        c = ContentManager.valueToContent(24);
+        wp = new WriteProperty("123456","test", c);
     }
 
     @After
@@ -18,13 +24,16 @@ public class WritePropertyTest {
 
     @Test
     public void getThingId() {
+        assertEquals("123456",wp.getThingId());
     }
 
     @Test
     public void getName() {
+        assertEquals("test",wp.getName());
     }
 
     @Test
-    public void getPayload() {
+    public void getPayload() throws ContentCodecException {
+        assertEquals(c,wp.getPayload());
     }
 }
