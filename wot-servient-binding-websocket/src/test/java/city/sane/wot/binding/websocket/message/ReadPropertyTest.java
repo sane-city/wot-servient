32ce96f9daddd90d6c377bf7d6102a39b7422ab6
Lukas von Thun <@8thun>
2019-12-16 14:57:02 +0100

    ReadProperty positiv

--- wot-servient-binding-websocket/src/test/java/city/sane/wot/binding/websocket/message/ReadPropertyTest.java
+++ wot-servient-binding-websocket/src/test/java/city/sane/wot/binding/websocket/message/ReadPropertyTest.java
@@ -7,9 +7,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ReadPropertyTest {
+    private ReadProperty rp;
+    private ReadProperty rp2;
+    private ReadProperty rp3;
+    private ReadProperty rp4;
+    private ReadProperty rp5;
 
     @Before
     public void setUp() throws Exception {
+        rp = new ReadProperty("123456","test");
+        rp2 = new ReadProperty(null,"test");
+
+
     }
 
     @After
@@ -18,9 +27,13 @@ public class ReadPropertyTest {
 
     @Test
     public void getThingId() {
+        assertEquals("123456",rp.getThingId());
+        assertNotNull(rp.getThingId());
     }
 
     @Test
     public void getName() {
+        assertEquals("test",rp.getName());
+        assertNotNull(rp.getName());
     }
 }
