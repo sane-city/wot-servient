eb67f142a40fb4c589b9c15a4c80b1d140f70bb2
Lukas von Thun <@8thun>
2019-12-16 15:21:50 +0100

    sp positiv

--- wot-servient-binding-websocket/src/test/java/city/sane/wot/binding/websocket/message/SubscribePropertyTest.java
+++ wot-servient-binding-websocket/src/test/java/city/sane/wot/binding/websocket/message/SubscribePropertyTest.java
@@ -7,9 +7,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SubscribePropertyTest {
+    SubscribeProperty sp;
 
     @Before
     public void setUp() throws Exception {
+        sp = new SubscribeProperty("123456","test");
     }
 
     @After
@@ -18,9 +20,13 @@ public class SubscribePropertyTest {
 
     @Test
     public void getThingId() {
+        assertEquals("123456",sp.getThingId());
+        assertNotNull(sp.getThingId());
     }
 
     @Test
     public void getName() {
+        assertEquals("test",sp.getName());
+        assertNotNull(sp.getName());
     }
 }
