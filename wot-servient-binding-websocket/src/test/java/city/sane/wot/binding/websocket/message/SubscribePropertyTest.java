48cbf88f523438be01adff1fe4659d040047d68c
Lukas von Thun <@8thun>
2019-12-16 13:50:57 +0100

    leere testklassen

---
+++ wot-servient-binding-websocket/src/test/java/city/sane/wot/binding/websocket/message/SubscribePropertyTest.java
@@ -0,0 +1,26 @@
+package city.sane.wot.binding.websocket.message;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SubscribePropertyTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getThingId() {
+    }
+
+    @Test
+    public void getName() {
+    }
+}
---
+++ wot-servient-binding-websocket/src/test/java/city/sane/wot/binding/websocket/message/WritePropertyTest.java
@@ -0,0 +1,30 @@
+package city.sane.wot.binding.websocket.message;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WritePropertyTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getThingId() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getPayload() {
+    }
+}
