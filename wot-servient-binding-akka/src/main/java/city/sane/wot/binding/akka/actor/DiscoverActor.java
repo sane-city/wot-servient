8a63568cb1bf46a5f90c526f6670f6ada928b313
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-01-08 18:59:19 +0100

    Change visibility

--- wot-servient-binding-akka/src/main/java/city/sane/wot/binding/akka/actor/DiscoverActor.java
+++ wot-servient-binding-akka/src/main/java/city/sane/wot/binding/akka/actor/DiscoverActor.java
@@ -19,7 +19,7 @@ import java.util.Map;
 /**
  * This actor is temporarily created for a discovery process. The actor searches for the desired things, returns them, and then terminates itself.
  */
-class DiscoverActor extends AbstractActor {
+public class DiscoverActor extends AbstractActor {
     private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
     private final Cancellable timer;
     private final ActorRef requester;
@@ -89,13 +89,13 @@ class DiscoverActor extends AbstractActor {
     }
 
     // CrudMessages
-    static class DiscoverTimeout {
+    public static class DiscoverTimeout {
 
     }
 
     public static class Done {
-        final ActorRef requester;
-        final Map<String, Thing> things;
+        public final ActorRef requester;
+        public final Map<String, Thing> things;
 
         public Done(ActorRef requester, Map<String, Thing> things) {
             this.requester = requester;
