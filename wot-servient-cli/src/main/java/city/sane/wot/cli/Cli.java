cd7ab6e2a0f007f39e4c9c5f28e416511273aed3
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-01-29 00:04:00 +0100

    Fix test

--- wot-servient-cli/src/main/java/city/sane/wot/cli/Cli.java
+++ wot-servient-cli/src/main/java/city/sane/wot/cli/Cli.java
@@ -110,14 +110,19 @@ class Cli {
         }
 
         // wait for all scripts to complete
-        completionFutures.forEach(future -> {
-            try {
-                future.get();
-            }
-            catch (InterruptedException | ExecutionException | CancellationException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        try {
+            completionFutures.forEach(future -> {
+                try {
+                    future.get();
+                }
+                catch (InterruptedException | ExecutionException | CancellationException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        catch (RuntimeException e) {
+            throw new CliException(e);
+        }
 
         // Shutdown servient if we are in client-only mode and all scripts have been executed, otherwise wait for termination by user
         if (cmd.hasOption(OPT_CLIENTONLY)) {
