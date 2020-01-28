2fe63837df25b9ac59011e4856c54f27328e8255
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-01-28 20:39:53 +0100

    Every exception inherited from WotException

--- wot-servient/src/main/java/city/sane/wot/binding/ProtocolServerException.java
+++ wot-servient/src/main/java/city/sane/wot/binding/ProtocolServerException.java
@@ -1,9 +1,11 @@
 package city.sane.wot.binding;
 
+import city.sane.wot.ServientException;
+
 /**
  * A ProtocolServerException is thrown by {@link ProtocolServer} implementations when errors occur.
  */
-public class ProtocolServerException extends Exception {
+public class ProtocolServerException extends ServientException {
     public ProtocolServerException(String message) {
         super(message);
     }
--- wot-servient/src/main/java/city/sane/wot/thing/observer/SubjectException.java
+++ wot-servient/src/main/java/city/sane/wot/thing/observer/SubjectException.java
@@ -1,9 +1,11 @@
 package city.sane.wot.thing.observer;
 
+import city.sane.wot.ServientException;
+
 /**
  * Will be thrown if interaction with a {@link Subject} is faulty (e.g. if an attempt is made to send new values to a closed subject).
  */
-abstract class SubjectException extends Exception {
+abstract class SubjectException extends ServientException {
     SubjectException(String message) {
         super(message);
     }
