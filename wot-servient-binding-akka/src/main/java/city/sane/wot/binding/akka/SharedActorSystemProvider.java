5b524d3fc040506b9ad3900ac218b2c11d0a1bb9
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-03-05 00:22:48 +0100

    Create things actor in shared actor system provider

--- wot-servient-binding-akka/src/main/java/city/sane/wot/binding/akka/SharedActorSystemProvider.java
+++ wot-servient-binding-akka/src/main/java/city/sane/wot/binding/akka/SharedActorSystemProvider.java
@@ -1,26 +1,40 @@
 package city.sane.wot.binding.akka;
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import city.sane.RefCountResource;
+import city.sane.Triple;
+import city.sane.wot.binding.akka.actor.ThingsActor;
+import city.sane.wot.thing.ExposedThing;
 import com.typesafe.config.Config;
 import scala.compat.java8.FutureConverters;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This is a Singleton class, which is used by {@link AkkaProtocolClient} and {@link
  * AkkaProtocolServer} to share a single ActorSystem.
  */
 public class SharedActorSystemProvider {
-    private static RefCountResource<ActorSystem> singleton = null;
+    private static RefCountResource<Triple<ActorSystem, Map<String, ExposedThing>, ActorRef>> singleton = null;
 
     private SharedActorSystemProvider() {
         // singleton class
     }
 
-    public static synchronized RefCountResource<ActorSystem> singleton(Config config) {
+    public static synchronized RefCountResource<Triple<ActorSystem, Map<String, ExposedThing>, ActorRef>> singleton(
+            Config config) {
         if (singleton == null) {
             singleton = new RefCountResource<>(
-                    () -> ActorSystem.create(config.getString("wot.servient.akka.system-name"), config.getConfig("wot.servient")),
-                    system -> FutureConverters.toJava(system.terminate()).toCompletableFuture().join()
+                    () -> {
+                        ActorSystem system = ActorSystem.create(config.getString("wot.servient.akka.system-name"), config.getConfig("wot.servient"));
+                        Map<String, ExposedThing> things = new HashMap<>();
+                        ActorRef thingsActor = system.actorOf(ThingsActor.props(things), "things");
+
+                        return new Triple(system, things, thingsActor);
+                    },
+                    triple -> FutureConverters.toJava(triple.first().terminate()).toCompletableFuture().join()
             );
         }
         return singleton;
