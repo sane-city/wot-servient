24918ef49c3963304a4e8000b1f9fb9b52a15e97
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-08-11 17:07:08 +0200

    Use different Singletons for different configurations
    See: https://github.com/sane-city/wot-servient/issues/2

--- wot-servient-binding-jadex/src/main/java/city/sane/wot/binding/jadex/SharedPlatformProvider.java
+++ wot-servient-binding-jadex/src/main/java/city/sane/wot/binding/jadex/SharedPlatformProvider.java
@@ -4,25 +4,30 @@ import city.sane.RefCountResource;
 import com.typesafe.config.Config;
 import jadex.bridge.IExternalAccess;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This is a Singleton class, which is used by {@link JadexProtocolClient} and {@link
  * JadexProtocolServer} to share a single Jadex Platform.
  */
 public class SharedPlatformProvider {
-    private static RefCountResource<IExternalAccess> singleton = null;
+    private static final Map<Config, RefCountResource<IExternalAccess>> singletons = new HashMap<>();
 
     private SharedPlatformProvider() {
         // singleton class
     }
 
     public static synchronized RefCountResource<IExternalAccess> singleton(Config config) {
-        if (singleton == null) {
-            JadexProtocolPlatformConfig jadexConfig = new JadexProtocolPlatformConfig(config);
-            singleton = new RefCountResource<>(
-                    () -> jadexConfig.createPlatform(),
-                    platform -> FutureConverters.fromJadex(platform.killComponent()).join()
-            );
-        }
-        return singleton;
+        return singletons.computeIfAbsent(
+                config,
+                myConfig -> new RefCountResource<>(
+                        () -> {
+                            JadexProtocolPlatformConfig jadexConfig = new JadexProtocolPlatformConfig(myConfig);
+                            return jadexConfig.createPlatform();
+                        },
+                        platform -> FutureConverters.fromJadex(platform.killComponent()).join()
+                )
+        );
     }
 }
--- wot-servient-binding-mqtt/src/main/java/city/sane/wot/binding/mqtt/SharedMqttClientProvider.java
+++ wot-servient-binding-mqtt/src/main/java/city/sane/wot/binding/mqtt/SharedMqttClientProvider.java
@@ -5,12 +5,15 @@ import city.sane.RefCountResource;
 import com.typesafe.config.Config;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This is a Singleton class, which is used by {@link MqttProtocolClient} and {@link
  * MqttProtocolServer} to share a single MqttClient.
  */
 public class SharedMqttClientProvider {
-    private static RefCountResource<Pair<MqttProtocolSettings, MqttClient>> singleton = null;
+    private static final Map<Config, RefCountResource<Pair<MqttProtocolSettings, MqttClient>>> singletons = new HashMap<>();
 
     private SharedMqttClientProvider() {
         // singleton class
@@ -18,16 +21,16 @@ public class SharedMqttClientProvider {
 
     public static synchronized RefCountResource<Pair<MqttProtocolSettings, MqttClient>> singleton(
             Config config) {
-        if (singleton == null) {
-            singleton = new RefCountResource<>(
-                    () -> {
-                        MqttProtocolSettings settings = new MqttProtocolSettings(config);
-                        settings.validate();
-                        return new Pair(settings, settings.createConnectedMqttClient());
-                    },
-                    pair -> pair.second().disconnect()
-            );
-        }
-        return singleton;
+        return singletons.computeIfAbsent(
+                config,
+                myConfig -> new RefCountResource<>(
+                        () -> {
+                            MqttProtocolSettings settings = new MqttProtocolSettings(myConfig);
+                            settings.validate();
+                            return new Pair<>(settings, settings.createConnectedMqttClient());
+                        },
+                        pair -> pair.second().disconnect()
+                )
+        );
     }
 }
