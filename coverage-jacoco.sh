ae33b7bb4ced5ce121a8d8aa50510d2ab3e3691f
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2021-01-21 18:22:40 +0100

    Copyright added

--- coverage-jacoco.sh
+++ coverage-jacoco.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
 
-# Collect all CSV files in one string
+#
+# Copyright (c) 2021.
+#
+# This file is part of SANE Web of Things Servient.
+#
+# SANE Web of Things Servient is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# SANE Web of Things Servient is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with SANE Web of Things Servient.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
 inputs=""
 for D in *; do
     if [ -d "${D}" ]; then
